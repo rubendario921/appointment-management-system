0a860381abf4a76d74a3bd5e4084f1da1b700a50
Rubén Darío Carrillo López <rubendario199201@gmail.com>
2026-02-10 13:27:19 -0500

    chore: install project dependencies.

---
+++ appointment-web/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { AppointmentPage } from './appointment-page/appointment-page';
+
+export const routes: Routes = [{ path: '', component: AppointmentPage }];
